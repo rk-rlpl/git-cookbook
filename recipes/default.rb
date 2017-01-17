2274b009cbe6a1fdcb3d8cf68d4dec1b1f7628fe
Raj Kiran Gade <raj.gade@relevancelab.com>
2017-01-17 15:01:31 +0530

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: git-cookbook
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "git"
+
+log "Well, that was easy!"
